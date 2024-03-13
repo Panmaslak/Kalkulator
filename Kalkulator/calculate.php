c8de44a7c9c262f11e550fe9911e6ba0e6096a99
Piotr Zań <73931821+Panmaslak@users.noreply.github.com>
2024-03-13 16:52:58 +0100

    Update calculate.php

--- Kalkulator/calculate.php
+++ Kalkulator/calculate.php
@@ -1,68 +1,58 @@
 <?php
 
+$error_message = '';
+
+function display_alert($message, $alert_type = 'danger') {
+    echo '<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">';
+    echo '<div class="container mt-5">';
+    echo '<div class="alert alert-' . $alert_type . '" role="alert">';
+    echo $message;
+    echo '</div>';
+    echo '<a href="/calculator/index.php" style="display: block;">Powrót</a>';
+    echo '</div>';
+}
+
 if (isset($_POST['submit'])) {
-  $num1 = $_POST['num1'];
-  $num2 = $_POST['num2'];
-  $operator = $_POST['operator'];
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $operator = $_POST['operator'];
 
-  if (!is_numeric($num1) || !is_numeric($num2)) {
-?>
-   <div class="container mt-5">
-        <div class="alert alert-error" role="alert">
-           Wprowadź prawidłowe numery         
-         </div>
-         <a href="/calculator/index.php" style="display: block;">Powrót</a>
-<?php
-    exit; 
-  }
+    if (!is_numeric($num1) || !is_numeric($num2)) {
+        $error_message = 'Wprowadź prawidłowe numery';
+    } else {
+        switch ($operator) {
+            case 'add':
+                $result = $num1 + $num2;
+                break;
+            case 'subtract':
+                $result = $num1 - $num2;
+                break;
+            case 'multiply':
+                $result = $num1 * $num2;
+                break;
+            case 'divide':
+                if ($num2 != 0) {
+                    $result = $num1 / $num2;
+                } else {
+                    $error_message = 'Nie można dzielić przez zero.';
+                }
+                break;
+            default:
+                $error_message = 'Nieznany operator.';
+        }
+    }
 
-  switch ($operator) {
-    case 'add':
-      $result = $num1 + $num2;
-      break;
-    case 'subtract':
-      $result = $num1 - $num2;
-      break;
-    case 'multiply':
-      $result = $num1 * $num2;
-      break;
-    case 'divide':
-      if ($num2 != 0) {
-        $result = $num1 / $num2;
-      } else {
-?>      
-   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-        <div class="container mt-5">
-        <div class="alert alert-danger" role="alert">
-           Nie można dzielić przez zero.         
-         </div>
-         <a href="/calculator/index.php" style="display: block;">Powrót</a>
-        </div>
-        <?php
-        exit; 
-      }
-      break;
-    default:
-    ?>
-        <div class="container mt-5">
-        <div class="alert alert-success" role="alert">
-            Nie wolno dzielić przez zero          
-        </div>
-        <a href="/calculator/index.php" style="display: block;">Powrót</a>
-    </div>
-
-    <?php
-      exit; 
-  }
-  ?>
-   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-     <div class="container mt-5">
-        <div class="alert alert-success" role="alert">
-            Wynik: <br><?php echo $result; ?>   
-        </div>
-        <a href="/calculator/index.php" style="display: block;">Powrót</a>
-    </div>
-<?php
+    if ($error_message == '') {
+        echo '<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">';
+        echo '<div class="container mt-5">';
+        echo '<div class="alert alert-success" role="alert">';
+        echo 'Wynik: <br>' . $result;
+        echo '</div>';
+        echo '<a href="/calculator/index.php" style="display: block;">Powrót</a>';
+        echo '</div>';
+    } else {
+        display_alert($error_message);
+    }
 }
 
 ?>
